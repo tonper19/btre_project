85cad792ffd2dc65a762f85486f4c6cc73d32abd
Tony Perez <tonper19@gmail.com>
2020-03-06 22:44:44 +0100

    Realtor Model & Run Migrations

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-from realtors.model import Realtor
+from realtors.models import Realtor
 
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
