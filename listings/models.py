5af0d27c2da9ddffe41ebddccf21ff51a9894f0b
Tony Perez <tonper19@gmail.com>
2020-03-07 17:42:38 +0100

    Media Folder & Adding Data

--- listings/models.py
+++ listings/models.py
@@ -27,4 +27,4 @@ class Listing(models.Model):
     list_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
-        return self.tittle
+        return self.title
