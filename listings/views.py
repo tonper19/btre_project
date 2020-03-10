41db4ebc9638760796681da15295338835b69d50
Tony Perez <tonper19@gmail.com>
2020-03-10 23:07:15 +0100

    46. Search Form Filtering

--- listings/views.py
+++ listings/views.py
@@ -24,13 +24,56 @@ def listing(request, listing_id):
     return render(request, "listings/listing.html", context)
 
 def search(request):
+    queryset_list = Listing.objects.order_by("-list_date")
+
+    # keyword, city, are names used in each input of search.html, if they 
+    # are filled, the request.GET will have them in it
+    # the i in front of icontains, iexact... stands for case insensitive
+
+    # Keywords for description (like '%bla bla bla%')
+    if "keywords" in request.GET:
+        keywords = request.GET["keywords"]
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City (exact match)
+    if "city" in request.GET:
+        city = request.GET["city"]
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State (exact match)
+    if "state" in request.GET:
+        state = request.GET["state"]
+        if state:
+            if state != "State (All)":
+                queryset_list = queryset_list.filter(state__iexact=state)
+
+    # bedrooms (less than or equal match)
+    if "bedrooms" in request.GET:
+        bedrooms = request.GET["bedrooms"]
+        if bedrooms:
+            try:
+                check_integer = int(bedrooms)
+                queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+            except ValueError:
+                pass  # ignore non numeric values and show them all
+
+    # price (less than or equal match)
+    if "price" in request.GET:
+        price = request.GET["price"]
+        if price:
+            try:
+                check_integer = int(price)
+                queryset_list = queryset_list.filter(price__lte=price)
+            except ValueError:
+                pass  # ignore non numeric values and show them all
 
     context = {
         "state_choices": state_choices,
         "bedroom_choices": bedroom_choices,
         "price_choices": price_choices,
+        "listings": queryset_list,
     }
-
-
     return render(request, "listings/search.html", context)
 
