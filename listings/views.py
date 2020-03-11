ce9c44905539d71dad45d5a420b5282f282b0e30
Tony Perez <tonper19@gmail.com>
2020-03-11 21:02:01 +0100

    47. Preserving Form Input

--- listings/views.py
+++ listings/views.py
@@ -74,6 +74,7 @@ def search(request):
         "bedroom_choices": bedroom_choices,
         "price_choices": price_choices,
         "listings": queryset_list,
+        "values": request.GET,
     }
     return render(request, "listings/search.html", context)
 
