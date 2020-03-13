34c6c266dc26510841a4a8576f16e63d0de57db1
Tony Perez <tonper19@gmail.com>
2020-03-13 21:44:01 +0100

    49. Rgister & Login Template

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
 
 def register(request):
+    if request.method == "POST":
+        # Register user logic goes here
+        print("*** (POST) registration submit coming soon!")
+        return redirect("register")
+
+    print("*** (GET) render registration.html")
     return render(request, "accounts/register.html")
 
 def login(request):
+    if request.method == "POST":
+        # Login user logic goes here
+        print("*** login submit coming soon!")
+        return redirect("login")
+
     return render(request, "accounts/login.html")
 
 def logout(request):
