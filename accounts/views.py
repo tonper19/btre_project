89fadaebbfd8aa60d0ade7c518ab1b048e731fdc
Tony Perez <tonper19@gmail.com>
2020-03-14 17:37:21 +0100

    51. User Registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == "POST":
-        # Register user logic goes here
-        print("*** (POST) registration submit coming soon!")
-        messages.error(request, "Error Message Test")
-        return redirect("register")
+        # Get form values
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password_again = request.POST["password2"]
+        
+        if password == password_again:
+            # check username on the DB
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username already exists")
+                return redirect("register")
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, "Email is being used")
+                    return redirect("register")
+                else:
+                    user = User.objects.create_user(username=username
+                                                    ,password=password
+                                                    ,email=email
+                                                    ,first_name=first_name
+                                                    ,last_name=last_name)
+                    # in case you need to login after register immediately:
+                    # auth.login(request, user)
+                    # messages.success(request, "You are now logged in")
+                    # return redirect("index")
 
-    print("*** (GET) render registration.html")
-    return render(request, "accounts/register.html")
+                    # Brad Traversy prefers the user logs in her/himself:
+                    user.save()
+                    messages.success(request, "You are now registered and can log in")
+                    return redirect("login")
+        else:
+            messages.error(request, "Passwords do not match")
+            return redirect("register")
+
+    else:
+        print("*** (GET) render registration.html")
+        return render(request, "accounts/register.html")
 
 def login(request):
     if request.method == "POST":
