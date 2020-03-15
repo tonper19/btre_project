1ce47c777d7e16ad96032d6cc274f4690f9f9fd2
Tony Perez <tonper19@gmail.com>
2020-03-15 22:22:04 +0100

    58. Contact Form Submission (2)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # each of these paths link to their specific method in accounts/views.py
     path("login", views.login, name="login"),
     path("register", views.register, name="register"),
     path("logout", views.logout, name="logout"),
